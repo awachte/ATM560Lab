75c36e8ab5ba5da8080c6112fdc409059a57cdca
nackan <nackan@tnstsremote.geips-amer.com>
2014-02-04 10:02:03 -0500

    Test commit

---
+++ ATM_RMI/src/atm/Server_Client_Interface.java
@@ -0,0 +1,12 @@
+package atm;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Server_Client_Interface extends Remote {
+	void deposit(int acnt, int amt)throws RemoteException;
+	void withdraw(int acnt, int amt)throws RemoteException;
+	void transfer(int acnt1, int acnt2, int amt)throws RemoteException;
+	int inquiry(int acnt)throws RemoteException;	
+
+}
