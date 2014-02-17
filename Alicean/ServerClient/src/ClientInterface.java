a99f565b86802ef594fb08ee6d06096921af9717
awachte <awachte@clemson.edu>
2014-02-17 13:26:42 -0500

    added remote class and interface.  moved some functions around.  followed javarmi.pdf tutorial.

--- Alicean/ServerClient/src/ClientInterface.java
+++ Alicean/ServerClient/src/ClientInterface.java
@@ -1,34 +1,13 @@
 import java.rmi.*;
 
-public class ClientStub {
-	String server;
-	int port;
-	
-	ClientStub(String server, int port)
-	{
-		this.server = server;
-		this.port = port;
-	}
-	
-	
+public interface ClientInterface extends Remote{
+	public String say(String msg) throws RemoteException;
 	// this operation increases the balance of user account acnt by amt, and returns nothing
-	void deposit(int acnt, int amt)
-	{
-		
-	}
+	void deposit(int acnt, int amt) throws RemoteException;
 	// this operation decreases the balance of user account acnt by amt, and returns nothing
-	void withdraw(int acnt, int amt)
-	{
-		
-	}
+	void withdraw(int acnt, int amt) throws RemoteException;
 	// this operation transfers money with amount of amt from banking account acnt1 to account acnt2
-	void transfer(int acnt1, int acnt2, int amt)
-	{
-		
-	}
+	void transfer(int acnt1, int acnt2, int amt) throws RemoteException;
 	// this operation returns the balance of user account acnt
-	int inquiry(int acnt)
-	{
-		return 0;
-	}
+	int inquiry(int acnt) throws RemoteException;
 }
