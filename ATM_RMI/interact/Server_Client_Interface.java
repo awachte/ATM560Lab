49305968dafdef0880639f60a5b616407720f4b8
andynack <andrew.nack@gmail.com>
2014-02-27 11:34:38 -0500

    debugged client

---
+++ ATM_RMI/interact/Server_Client_Interface.java
@@ -0,0 +1,16 @@
+package interact;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Server_Client_Interface extends Remote {
+	
+	void deposit(int acnt, int amt)throws RemoteException;
+	
+	void withdraw(int acnt, int amt)throws RemoteException;
+	
+	void transfer(int acnt1, int acnt2, int amt)throws RemoteException;
+	
+	int inquiry(int acnt)throws RemoteException;	
+
+}
---
+++ ATM_RMI/server/Server_Object.java
@@ -0,0 +1,207 @@
+package server;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+import interact.Server_Client_Interface;
+
+public class Server_Object implements Server_Client_Interface {
+
+	private List<String> accounts = new ArrayList<String>();
+	private List<String> balances = new ArrayList<String>();
+		
+	public void deposit(int acnt, int amt) throws RemoteException{
+		
+		try {
+			Path file = Paths.get("/home/andy/ATM_RMI/accounts.txt");
+			Charset charset = Charset.forName("UTF-8");
+			accounts = Files.readAllLines(file, charset);
+			
+		} catch (IOException x) {
+			System.err.println(x);
+		}
+		
+		try {
+			Path file = Paths.get("/home/andy/ATM_RMI/balances.txt");
+			Charset charset = Charset.forName("UTF-8");
+			balances = Files.readAllLines(file, charset);
+			
+		} catch (IOException x) {
+			System.err.println(x);
+		}
+
+		String a = String.valueOf(acnt);
+		
+		int index = accounts.indexOf(a);
+		if (index == -1){
+			System.out.println("Account" + acnt + "does not exist.");
+			return;
+		}
+		
+		int bal = (Integer.valueOf(balances.get(index))).intValue();
+		
+		String dpt = String.valueOf(amt + bal);
+		
+		balances.set(index, dpt);
+		
+		try {
+			Path file = Paths.get("/home/andy/ATM_RMI/balances.txt");
+			Charset charset = Charset.forName("UTF-8");
+			Files.write(file, balances, charset);
+			
+		} catch (IOException x) {
+			System.err.println(x);
+		}
+		
+		System.out.println("$" + amt + " successfully deposited into account " + acnt + ".");
+		
+	}
+
+	@Override
+	public void withdraw(int acnt, int amt) throws RemoteException{
+		
+		try {
+			Path file = Paths.get("/home/andy/ATM_RMI/accounts.txt");
+			Charset charset = Charset.forName("UTF-8");
+			accounts = Files.readAllLines(file, charset);
+			
+		} catch (IOException x) {
+			System.err.println(x);
+		}
+		
+		try {
+			Path file = Paths.get("/home/andy/ATM_RMI/balances.txt");
+			Charset charset = Charset.forName("UTF-8");
+			balances = Files.readAllLines(file, charset);
+			
+		} catch (IOException x) {
+			System.err.println(x);
+		}
+
+		String a = String.valueOf(acnt);
+		
+		int index = accounts.indexOf(a);
+		if (index == -1){
+			System.out.println("Account" + acnt + "does not exist.");
+			return;
+		}
+		
+		int bal = (Integer.valueOf(balances.get(index))).intValue();
+		
+		String wdl = String.valueOf(bal - amt);
+		
+		balances.set(index, wdl);
+		
+		try {
+			Path file = Paths.get("/home/andy/ATM_RMI/balances.txt");
+			Charset charset = Charset.forName("UTF-8");
+			Files.write(file, balances, charset);
+			
+		} catch (IOException x) {
+			System.err.println(x);
+		}
+		
+		System.out.println("$" + amt + " successfully withdrawn from account " + acnt + ".");
+	}
+
+	@Override
+	public void transfer(int acnt1, int acnt2, int amt) throws RemoteException{
+		
+		try {
+			Path file = Paths.get("/home/andy/ATM_RMI/accounts.txt");
+			Charset charset = Charset.forName("UTF-8");
+			accounts = Files.readAllLines(file, charset);
+			
+		} catch (IOException x) {
+			System.err.println(x);
+		}
+		
+		try {
+			Path file = Paths.get("/home/andy/ATM_RMI/balances.txt");
+			Charset charset = Charset.forName("UTF-8");
+			balances = Files.readAllLines(file, charset);
+			
+		} catch (IOException x) {
+			System.err.println(x);
+		}
+
+		String a = String.valueOf(acnt1);
+		String b = String.valueOf(acnt2);
+		
+		int index1 = accounts.indexOf(a);
+		if (index1 == -1){
+			System.out.println("Account" + acnt1 + "does not exist.");
+			return;
+		}
+		
+		int index2 = accounts.indexOf(b);
+		if (index2 == -1){
+			System.out.println("Account" + acnt2 + "does not exist.");
+			return;
+		}
+		
+		int bal1 = (Integer.valueOf(balances.get(index1))).intValue();
+		
+		int bal2 = (Integer.valueOf(balances.get(index2))).intValue();
+		
+		String wdl = String.valueOf(bal1 - amt);
+		
+		balances.set(index1, wdl);
+		
+		String dpt = String.valueOf(bal2 + amt);
+		
+		balances.set(index2, dpt);
+
+		try {
+			Path file = Paths.get("/home/andy/ATM_RMI/balances.txt");
+			Charset charset = Charset.forName("UTF-8");
+			Files.write(file, balances, charset);
+			
+		} catch (IOException x) {
+			System.err.println(x);
+		}
+				
+		System.out.println("$" + amt + " successfully transfered from account " + acnt1 + " to account " + acnt2 + ".");
+	}
+
+	@Override
+	public int inquiry(int acnt) throws RemoteException{
+		
+		try {
+			Path file = Paths.get("/home/andy/ATM_RMI/accounts.txt");
+			Charset charset = Charset.forName("UTF-8");
+			accounts = Files.readAllLines(file, charset);
+			
+		} catch (IOException x) {
+			System.err.println(x);
+		}
+		
+		try {
+			Path file = Paths.get("/home/andy/ATM_RMI/balances.txt");
+			Charset charset = Charset.forName("UTF-8");
+			balances = Files.readAllLines(file, charset);
+			
+		} catch (IOException x) {
+			System.err.println(x);
+		}
+
+		String a = String.valueOf(acnt);
+		
+		int index = accounts.indexOf(a);
+		if (index == -1){
+			System.out.println("Account" + acnt + "does not exist.");
+			return -1;
+		}
+		
+		int bal = (Integer.valueOf(balances.get(index))).intValue();
+							
+		System.out.println("The balance of account " + acnt + " is $" + bal + ".");
+		
+		return bal;
+	}
+}
